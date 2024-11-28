5272652c3d09077a2279d516f2e1e729da253576
Caio Gabriel <105945358+CaioGTF@users.noreply.github.com>
2024-11-28 03:18:27 +0000

    sustentatur

--- sustentatur/script.js
+++ sustentatur/script.js
@@ -10,3 +10,18 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+function seaarch() {
+    let input = document.getElementById("searchbarr").value;
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName("pesquisas");
+
+    for(i=0; i < x.length; i++) {
+        if(!x[i].innerHTML.toLowerCase().includes(input)){
+            x[i].style.display = "none"
+        }else {
+            x[i].style.display = "list-item"
+        }
+    }
+
+}
+
