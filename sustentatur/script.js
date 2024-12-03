8c2c90c3573cd2715f0159aca8a9c9fbfe1068ae
Caio Gabriel <105945358+CaioGTF@users.noreply.github.com>
2024-12-03 13:52:40 +0000

    sustentatur

--- sustentatur/script.js
+++ sustentatur/script.js
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
 let trilho = document.getElementById("trilho");
 let body = document.querySelector('body');
 
+
 trilho.addEventListener('click', ()=>{
     trilho.classList.toggle('dark')
     body.classList.toggle('dark')
